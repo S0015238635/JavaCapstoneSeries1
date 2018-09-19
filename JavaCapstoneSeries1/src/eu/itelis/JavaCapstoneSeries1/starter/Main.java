112ad33095f76ed63c87251f4898dbcc780d4354
Elvis Marion <elvis.marion@actual-it.si>
2018-09-19 22:35:39 +0200

    Creating link Eclipse - GitHub

---
+++ JavaCapstoneSeries1/src/eu/itelis/JavaCapstoneSeries1/starter/Main.java
@@ -0,0 +1,15 @@
+package eu.itelis.JavaCapstoneSeries1.starter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i = 0;
+		// TODO Auto-generated method stub
+		System.out.println("Hello Elvis!");
+		do {
+			i++;
+			System.out.println(i);
+		} while (i < 10);
+	}
+
+}
